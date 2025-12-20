e78a16f7126676f810eddaa4c3bde9b9decc2d89
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-20 17:11:48 +0100

    Add 2015/1.py

---
+++ 2015/1.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+day = Path(__file__).stem
+input_file = f'{day}.txt'
+input_str = open(input_file).read()
+
+p1 = 0
+p2 = 0
+for c in input_str:
+    if c == '(':
+        p1 += 1
+    elif c == ')':
+        p1 -= 1
+    p2 += 1
+    if p1 == -1:
+        break
+
+print(p1)
+print(p2)
