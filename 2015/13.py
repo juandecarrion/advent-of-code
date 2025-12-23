0697c8f61a57c8c87d96c3da40a1689f2b2537c3
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-23 18:55:22 +0100

    Add 2015/13.py

--- 2015/13.py
+++ 2015/13.py
@@ -1,25 +1,57 @@
+import itertools
 from pathlib import Path
 
-TEST = True
+import numpy as np
+
+TEST = False
 
 day = Path(__file__).stem
 input_file = f"{day}.test.txt" if TEST else f"{day}.txt"
 input_str = open(input_file).read().rstrip()
 lines = input_str.split("\n")
 
-p1 = 0
-p2 = 0
 
-# Bob would gain 83 happiness units by sitting next to Alice.
-# Bob would lose 7 happiness units by sitting next to Carol.
-guests = []
-for line in lines:
-    words = line.rstrip(".").split()
-    if words[0] not in guests:
-        guests.append(words[0])
-    if words[10] not in guests:
-        guests.append(words[10])
+def solve(add_me=False):
+    guests = []
+    for line in lines:
+        words = line.rstrip(".").split()
+        if words[0] not in guests:
+            guests.append(words[0])
+        if words[10] not in guests:
+            guests.append(words[10])
+    if add_me:
+        guests.append("me")
 
+    a = np.zeros((len(guests), len(guests)))
+
+    for line in lines:
+        words = line.rstrip(".").split()
+        guest_i = guests.index(words[0])
+        guest_j = guests.index(words[10])
+        hu = int(words[3])
+        if words[2] == "lose":
+            hu *= -1
+        a[guest_i][guest_j] = hu
+
+    arrangements = list(itertools.permutations(range(len(guests))))
+
+    result = 0
+    for arrangement in arrangements:
+        left_arrangement = arrangement[1:] + (arrangement[0],)
+        right_arrangement = (arrangement[-1],) + arrangement[:-1]
+        happiness = 0
+        for left, center, right in zip(
+            left_arrangement, arrangement, right_arrangement
+        ):
+            happiness += a[center][left]
+            happiness += a[center][right]
+        result = int(max(result, happiness))
+
+    return result
+
+
+p1 = solve(add_me=False)
+p2 = solve(add_me=True)
 
 print(p1)
 print(p2)
