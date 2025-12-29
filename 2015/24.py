7348c8b84c0a9c93569987d0bd51c3f564025b6d
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-30 00:33:45 +0100

    Add 2015/24.py

--- 2015/24.py
+++ 2015/24.py
@@ -21,40 +21,71 @@ packages = []
 for line in lines:
     packages.append(int(line))
 
-total_weight = sum(packages)
-weight_per_group = total_weight // 3
 
-ITERATION_FACTOR = 2**20
-options = set()
-for i in range(1, 7):
-    for _ in range(ITERATION_FACTOR):
-        random.shuffle(packages)
-        first_group = packages[:i]
-        if sum(first_group) == weight_per_group:
-            rest = packages[i:]
-            for _ in range(ITERATION_FACTOR):
-                random.shuffle(rest)
-                for j in range(1, len(rest) - 1):
-                    second_group = sum(rest[:j])
-                    third_group = sum(rest[j:])
-                    if second_group == third_group == weight_per_group:
-                        options.add(tuple(sorted(first_group)))
-    if options:
-        break
+def solve(no_of_groups):
+    total_weight = sum(packages)
+    weight_per_group = total_weight // no_of_groups
 
-p1 = sys.maxsize
-# p1 = 270894973147
-# p1 = 102937852963
-# p1 = 48585083935
-# p1 = 30367698987
-# p1 = 29728298883
-# p1 = 10542541903
-# p1 = 10439961859
-for option in options:
-    entanglement = reduce(mul, option)
-    if entanglement <= p1:
-        print(len(option), entanglement, option)
-        p1 = min(p1, entanglement)
+    ITERATION_FACTOR = 2**20
+    options = set()
+    for i in range(1, len(packages)):
+        for _ in range(ITERATION_FACTOR):
+            random.shuffle(packages)
+            first_group = packages[:i]
+            if sum(first_group) == weight_per_group:
+                rest = packages[i:]
+                for _ in range(ITERATION_FACTOR):
+                    matches = False
+                    random.shuffle(rest)
+                    if no_of_groups == 3:
+                        for j in range(1, len(rest) - 1):
+                            second_group = rest[:j]
+                            third_group = rest[j:]
+                            if (
+                                sum(second_group)
+                                == sum(third_group)
+                                == weight_per_group
+                            ):
+                                for group in [first_group, second_group, third_group]:
+                                    if len(group) == i:
+                                        options.add(tuple(sorted(group)))
+                                matches = True
+                    else:
+                        for j in range(1, len(rest) - 2):
+                            for k in range(j + 1, len(rest) - 1):
+                                second_group = rest[:j]
+                                third_group = rest[j:k]
+                                fourth_group = rest[k:]
+                                if (
+                                    sum(second_group)
+                                    == sum(third_group)
+                                    == sum(fourth_group)
+                                    == weight_per_group
+                                ):
+                                    for group in [
+                                        first_group,
+                                        second_group,
+                                        third_group,
+                                        fourth_group,
+                                    ]:
+                                        if len(group) == i:
+                                            options.add(tuple(sorted(group)))
+                                    matches = True
+                    if matches:
+                        break
+        if options:
+            break
+
+    result = sys.maxsize
+    for option in options:
+        entanglement = reduce(mul, option)
+        result = min(result, entanglement)
+
+    return result
+
+
+p1 = solve(no_of_groups=3)
+p2 = solve(no_of_groups=4)
 
 print(p1)
 print(p2)
