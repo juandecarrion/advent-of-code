bc90b01e607bba546a38ec914c0392991c73823a
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-30 16:25:05 +0100

    Add 2015/25.py

--- 2015/25.py
+++ 2015/25.py
@@ -1,6 +1,7 @@
+import re
 from pathlib import Path
 
-TEST = False
+TEST = True
 print(f"{TEST=}")
 
 day = Path(__file__).stem
@@ -12,7 +13,96 @@ lines = input_str.split("\n")
 p1 = 0
 p2 = 0
 
+code_row, code_column = re.findall(r"\d+", lines[0])
+code_pos = (int(code_row), int(code_column))
+
+input_str = open("25.manual.txt").read().rstrip()
+lines = input_str.split("\n")
+
+
+offset = 2
+codes = []
+for i_with_offset in range(offset, len(lines)):
+    codes.append([])
+    _, line = lines[i_with_offset].split("|")
+    i = i_with_offset - offset
+    for j, code in enumerate(line.split()):
+        codes[-1].append(int(code))
+
 pass
+
+
+def get_next_pos(pos):
+    x, y = pos
+    return (x - 1, y + 1) if x > 1 else (y + 1, 1)
+
+
+def test_get_next_pos(pos, next_pos):
+    assert get_next_pos(pos) == next_pos
+
+
+"""
+   | 1   2   3   4   5   6  
+---+---+---+---+---+---+---+
+ 1 |  1   3   6  10  15  21
+ 2 |  2   5   9  14  20
+ 3 |  4   8  13  19
+ 4 |  7  12  18
+ 5 | 11  17
+ 6 | 16
+"""
+
+if TEST:
+    test_get_next_pos((1, 1), (2, 1))
+    test_get_next_pos((2, 1), (1, 2))
+    test_get_next_pos((1, 2), (3, 1))
+    test_get_next_pos((3, 1), (2, 2))
+    test_get_next_pos((2, 2), (1, 3))
+    test_get_next_pos((1, 3), (4, 1))
+    test_get_next_pos((4, 1), (3, 2))
+    test_get_next_pos((3, 2), (2, 3))
+    test_get_next_pos((2, 3), (1, 4))
+
+
+def get_next_value(value):
+    result = value * 252533
+    result = result % 33554393
+    return result
+
+
+def test_get_next_val(value, next_value):
+    assert get_next_value(value) == next_value
+
+
+"""
+   |    1         2         3         4         5         6
+---+---------+---------+---------+---------+---------+---------+
+ 1 | 20151125  18749137  17289845  30943339  10071777  33511524
+ 2 | 31916031  21629792  16929656   7726640  15514188   4041754
+ 3 | 16080970   8057251   1601130   7981243  11661866  16474243
+ 4 | 24592653  32451966  21345942   9380097  10600672  31527494
+ 5 |    77061  17552253  28094349   6899651   9250759  31663883
+ 6 | 33071741   6796745  25397450  24659492   1534922  27995004
+"""
+
+
+if TEST:
+    test_get_next_val(20151125, 31916031)
+    test_get_next_val(31916031, 18749137)
+    test_get_next_val(18749137, 16080970)
+    test_get_next_val(16080970, 21629792)
+    test_get_next_val(21629792, 17289845)
+    test_get_next_val(17289845, 24592653)
+
+
+pos = (1, 1)
+value = codes[0][0]
+while True:
+    pos = get_next_pos(pos)
+    value = get_next_value(value)
+    if pos == code_pos:
+        p1 = value
+        break
 
 
 print(p1)
