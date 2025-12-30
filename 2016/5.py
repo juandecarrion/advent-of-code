59f58eb98635a10f76e58de477179c6c5fb324ea
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-30 22:36:58 +0100

    Add 2016/5.py

--- 2016/5.py
+++ 2016/5.py
@@ -1,6 +1,7 @@
+import hashlib
 from pathlib import Path
 
-TEST = False
+TEST = True
 print(f"{TEST=}")
 
 day = Path(__file__).stem
@@ -8,11 +9,23 @@ input_file = f"{day}.test.txt" if TEST else f"{day}.txt"
 input_str = open(input_file).read().rstrip()
 lines = input_str.split("\n")
 
-p1 = 0
-p2 = 0
+p1 = ""
+p2 = "????????"
 
-pass
-
+i = 0
+while True:
+    i += 1
+    to_hash = input_str + str(i)
+    hash_str = hashlib.md5(to_hash.encode()).hexdigest()
+    if hash_str.startswith("00000"):
+        if len(p1) < 8:
+            p1 += hash_str[5]
+        if hash_str[5].isdigit():
+            index = int(hash_str[5])
+            if 0 <= index <= 7 and p2[index] == "?":
+                p2 = p2[:index] + hash_str[6] + p2[index + 1 :]
+    if len(p1) >= 8 and "?" not in p2:
+        break
 
 print(p1)
 print(p2)
