23f1e5000bc627895aa4b3fcb67fc13e91bb21cd
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-30 13:57:31 +0100

    Add 2015/25.py

---
+++ 2015/25.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+TEST = False
+print(f"{TEST=}")
+
+day = Path(__file__).stem
+input_file = f"{day}.test.txt" if TEST else f"{day}.txt"
+input_str = open(input_file).read().rstrip()
+lines = input_str.split("\n")
+
+
+p1 = 0
+p2 = 0
+
+pass
+
+
+print(p1)
+print(p2)
+pass
