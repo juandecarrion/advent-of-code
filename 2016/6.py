d30a241c3919321bf3a00bac4dc03547acb5fbdb
Juande Carrión <971417+juandecarrion@users.noreply.github.com>
2025-12-30 22:44:45 +0100

    Add 2016/6.py

--- 2016/6.py
+++ 2016/6.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-TEST = True
+TEST = False
 print(f"{TEST=}")
 
 day = Path(__file__).stem
@@ -8,10 +8,27 @@ input_file = f"{day}.test.txt" if TEST else f"{day}.txt"
 input_str = open(input_file).read().rstrip()
 lines = input_str.split("\n")
 
-p1 = 0
-p2 = 0
+p1 = ""
+p2 = ""
 
-pass
+frequency = []
+for i in lines[0]:
+    frequency.append({})
+
+for line in lines:
+    for j, char in enumerate(line):
+        if char not in frequency[j]:
+            frequency[j][char] = 1
+        else:
+            frequency[j][char] += 1
+
+for column in frequency:
+    items = list(column.items())
+    items.sort(key=lambda x: x[1], reverse=True)
+    p1 += str(items[0][0])
+    p2 += str(items[-1][0])
+    pass
+
 
 print(p1)
 print(p2)
